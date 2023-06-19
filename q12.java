8101e435a7d75945e45332277fdf081f67d9c144
sonu-ku05 <122732990+sonu-ku05@users.noreply.github.com>
2023-06-19 18:02:59 +0530

    Add files via upload
    Version1 Q12.java

---
+++ q12.java
@@ -0,0 +1,16 @@
+package q12;
+
+import java.util.Scanner;
+
+public class q12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("what is your name");
+   Scanner Sc = new Scanner (System.in);
+   String name = Sc.next();
+   System.out.println("hello" + name + "have a good day !");
+   
+	}
+
+}
